4ca46e52cfa075c14e608ab9511b537bde7e6007
Dave Emmanuel Nuada <davenuada025@gmail.com>
2025-07-25 16:04:25 +0800

    Showing data on the frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,24 @@ import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 import { UploadButton } from "~/utils/uploadthing";
 import { UploadDialog } from "./_components/upload-dialog";
+import { getMyImages } from "~/server/queries";
+
+export const dynamic = "force-dynamic"; // This page is dynamic and should not be cached
 
 async function Images() {
-  const mockUrls = [
-    "https://www.jedishop.eu/_obchody/www.jedishop.cz/prilohy/675/character-vocal-series-01-hatsune-miku-pvc-soska-1.jpg.big.jpg",
-    "https://images.immediate.co.uk/production/volatile/sites/3/2025/01/hatsune-miku-fortnite-e23a13b.jpg?resize=1366,910",
-    "https://bm1mkaq6i6.ufs.sh/f/vVnKMogA5mBcE7CgZBTbQsL2P64R0Z5wX3hpB7rqIxiyukSo",
-    "https://bm1mkaq6i6.ufs.sh/f/vVnKMogA5mBccqOC7MK0LE236HjuyDFnkZgl98warKhezicN",
-  ];
+  // const mockUrls = [
+  //   "https://www.jedishop.eu/_obchody/www.jedishop.cz/prilohy/675/character-vocal-series-01-hatsune-miku-pvc-soska-1.jpg.big.jpg",
+  //   "https://images.immediate.co.uk/production/volatile/sites/3/2025/01/hatsune-miku-fortnite-e23a13b.jpg?resize=1366,910",
+  //   "https://bm1mkaq6i6.ufs.sh/f/vVnKMogA5mBcE7CgZBTbQsL2P64R0Z5wX3hpB7rqIxiyukSo",
+  //   "https://bm1mkaq6i6.ufs.sh/f/vVnKMogA5mBccqOC7MK0LE236HjuyDFnkZgl98warKhezicN",
+  // ];
 
-  const images = mockUrls.map((url, index) => ({
-    id: index + 1,
-    url,
-  }));
+  // const images = mockUrls.map((url, index) => ({
+  //   id: index + 1,
+  //   url,
+  // }));
+
+  const images = await getMyImages();
 
   return (
     <div>
@@ -26,7 +31,7 @@ async function Images() {
           <div key={image.id} className="flex w-64 flex-col">
             <div className="relative aspect-video bg-zinc-900">
               <img
-                src={image.url}
+                src={image.imageUrl}
                 alt={`Image ${image.id}`}
                 className="h-full w-full object-contain object-top"
               />
@@ -39,7 +44,7 @@ async function Images() {
   );
 }
 
-export default function HomePage() {
+export default async function HomePage() {
   return (
     <main className="">
       <SignedOut>
